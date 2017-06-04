b652f89fa55f5e10cd11414a481c8aa0f022b339
yanyg <yygcode@gmail.com>
2017-06-04 09:31:23 +0800

    add ivy

--- init.el
+++ init.el
@@ -1,32 +1,45 @@
-;; -*- coding: utf-8 -*-
-;;
-;; Copyright (C) yanyg<yygcode@gmail.com>
-;;
+;;; init.el --- emacs config entry
+
+;; Copyright (C) 2017 yanyg<yygcode@gmail.com>
+
+;; Author: yanyg<yygcode@gmail.com>
+;; Homepage: http://ycode.org
+
+;;; Commentary:
+
+;;; Code:
+
+;; If you want to use the latest, config org as belows:
+;; 1. Download latest package or clone repo.
+;;    URL: http://orgmode.org/
+;;    REPO:
+;;      ~$ git clone git://orgmode.org/org-mode.git
+;;      ~$ make autoloads
+;; 2. add load-path
+;;    (add-to-list 'load-path "~/path/to/orgdir/lisp")
+;; 3. If you wantt contributed libraries
+;;    (add-to-list 'load-path "~/path/to/orgdir/contrib/lisp" t)
+;; See homepage http://orgmode.org/ for more details.
+
+(org-babel-load-file "~/.emacs.d/config.org")
 
 ;; use china mirror for GFW
-(require 'package)
-(setq package-archives '(("gnu" . "http://elpa.emacs-china.org/gnu/")
-			 ("melpa" . "http://elpa.emacs-china.org/melpa/")))
-;;(setq package-archives
-;;      '(("gnu" . "file:///home/yanyg/mnt/home/emacs-local/elpa/gnu")
-;;	("melpa" . "http:///home/yanyg/mnt/home/emacs-local/elpa/melpa")))
-(package-initialize)
 
-;; store custom variables separately
-(setq custom-file "~/.emacs.d/custom.el")
-(when (file-exists-p custom-file) (load custom-file))
+;; ;; store custom variables separately
+;; (setq custom-file "~/.emacs.d/custom.el")
+;; (when (file-exists-p custom-file) (load custom-file))
 
-;;(setq inhibit-splash-screen t)
+;; ;;(setq inhibit-splash-screen t)
 
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
+;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
-;;(shell-command-to-string "fortune")
+;; ;;(shell-command-to-string "fortune")
 
-(require 'init-packages)
-(require 'init-basic)
-(require 'init-linum)
-(require 'init-org)
-(require 'init-chinese-pyim)
-;;(setq frame-resize-pixelwise 1)
-;;(toggle-frame-fullscreen)
-;;(toggle-frame-fullscreen)
+;; (require 'init-packages)
+;; (require 'init-basic)
+;; (require 'init-linum)
+;; (require 'init-org)
+;; (require 'init-chinese-pyim)
+;; ;;(setq frame-resize-pixelwise 1)
+;; ;;(toggle-frame-fullscreen)
+;; ;;(toggle-frame-fullscreen)
