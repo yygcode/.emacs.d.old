5cc775ca9d55f4ee7a6ef3f6dd0077924f85e9fa
yanyg <yygcode@gmail.com>
2017-06-14 00:24:39 +0800

    Merge custom.el

--- custom.el
+++ custom.el
@@ -5,11 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-<<<<<<< HEAD
     (projectile zygospore ycmd window-numbering use-package solarized-theme smartparens powerline pdf-tools org-pomodoro org-page org-bullets nyan-mode mwe-log-commands monokai-theme iedit hungry-delete helm-gtags ggtags function-args flycheck fill-column-indicator expand-region evil-leader disable-mouse counsel company command-log-mode color-theme-solarized chinese-pyim-greatdict chinese-pyim chinese-fonts-setup 2048-game))))
-=======
-    (sr-speedbar youdao-dictionary ycmd w3m use-package smex smartparens sdcv powerline pdf-tools org-pomodoro org-page org-bullets mpg123 monokai-theme iedit hungry-delete helm-ls-git helm-gtags helm-firefox ggtags geben-helm-projectile flycheck fill-column-indicator expand-region evil eshell-git-prompt disable-mouse counsel company column-marker color-theme-solarized chinese-pyim-greatdict chinese-pyim chinese-fonts-setup))))
->>>>>>> 29286246f870332498158d2278d9ed7073e1bd66
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
