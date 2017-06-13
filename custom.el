03ea88117705b86309050dab9a2359ac1bcd21a3
yanyg <yygcode@gmail.com>
2017-06-14 01:02:36 +0800

    daily commit

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (projectile zygospore ycmd window-numbering use-package solarized-theme smartparens powerline pdf-tools org-pomodoro org-page org-bullets nyan-mode mwe-log-commands monokai-theme iedit hungry-delete helm-gtags ggtags function-args flycheck fill-column-indicator expand-region evil-leader disable-mouse counsel company command-log-mode color-theme-solarized chinese-pyim-greatdict chinese-pyim chinese-fonts-setup 2048-game))))
+    (anzu ws-butler dtrt-indent clean-aindent-mode yasnippet volatile-highlights zygospore ycmd window-numbering use-package solarized-theme smartparens powerline pdf-tools org-pomodoro org-page org-bullets nyan-mode mwe-log-commands monokai-theme iedit hungry-delete helm-swoop helm-projectile helm-gtags ggtags function-args flycheck fill-column-indicator expand-region evil-leader disable-mouse counsel company-c-headers command-log-mode color-theme-solarized chinese-pyim-greatdict chinese-pyim chinese-fonts-setup 2048-game))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
